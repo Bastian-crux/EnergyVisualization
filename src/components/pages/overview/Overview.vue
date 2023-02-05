34a1f79a8e7714894b7b002253a6991eceb3488a
Shawn Huang <shawn.huang.y.h@gmail.com>
2023-02-05 22:00:31 +0800

    feat: add energy selector

--- src/components/pages/overview/Overview.vue
+++ src/components/pages/overview/Overview.vue
@@ -1,19 +1,44 @@
 <template>
   <base-card>
-    <el-button>test</el-button>
+    <div>
+      能源类型
+    </div>
+    <div>
+      <el-radio-group v-model="radio" style="margin-top: 20px">
+        <el-radio-button label="solar">太阳能</el-radio-button>
+        <el-radio-button label="wind">风能</el-radio-button>
+        <el-radio-button label="nuclear">核能</el-radio-button>
+      </el-radio-group>
+    </div>
   </base-card>
   <base-card>
-    <map-visualization></map-visualization>
+    <map-visualization :energy-type="energyType"></map-visualization>
   </base-card>
 </template>
 
 <script>
 import BaseCard from "@/components/UI/BaseCard.vue";
 import MapVisualization from "@/components/pages/overview/MapVisualization.vue";
+import {ref} from 'vue';
 
 export default {
   name: "Overview",
-  components: {MapVisualization, BaseCard}
+  components: {MapVisualization, BaseCard},
+
+  data() {
+    return {
+      radio: ref('solar'),
+      energyType: 'solar',
+    }
+  },
+  methods:{
+
+  },
+  watch: {
+    radio: function (val) {
+      this.energyType = val;
+    }
+  }
 }
 </script>
 
